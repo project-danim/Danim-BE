369e015f8bc13a4911a0685feb7da225d2504871
youngkikim14 <127404498+youngkikim14@users.noreply.github.com>
2023-06-10 23:34:45 +0900

    Update WebSecurityConfig.java

--- src/main/java/com/project/danim_be/security/config/WebSecurityConfig.java
+++ src/main/java/com/project/danim_be/security/config/WebSecurityConfig.java
@@ -23,7 +23,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@CrossOrigin("\*")
 public class WebSecurityConfig {
 
 	private final JwtAuthenticationFilter jwtUtil;
