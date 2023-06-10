26cb2cd707139b296c1fdbd47a1388fa00e63c64
youngkikim14 <127404498+youngkikim14@users.noreply.github.com>
2023-06-11 00:41:11 +0900

    Update WebMvcConfig.java

--- src/main/java/com/project/danim_be/security/config/WebMvcConfig.java
+++ src/main/java/com/project/danim_be/security/config/WebMvcConfig.java
@@ -1,18 +1,18 @@
- package com.project.danim_be.security.config;
+//  package com.project.danim_be.security.config;
 
 
- import org.springframework.context.annotation.Configuration;
- import org.springframework.web.servlet.config.annotation.CorsRegistry;
- import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//  import org.springframework.context.annotation.Configuration;
+//  import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//  import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
- @Configuration
- public class WebMvcConfig implements WebMvcConfigurer {
- 	@Override
- 	public void addCorsMappings(CorsRegistry registry) {
- 		registry.addMapping("/**")
- 			.allowedOrigins("https://da-nim.com:443")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-        ;
- 	}
- }
+//  @Configuration
+//  public class WebMvcConfig implements WebMvcConfigurer {
+//  	@Override
+//  	public void addCorsMappings(CorsRegistry registry) {
+//  		registry.addMapping("/**")
+//  			.allowedOrigins("https://da-nim.com:443")
+//                 .allowedMethods("*")
+//                 .allowedHeaders("*")
+//         ;
+//  	}
+//  }
